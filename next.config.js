555e062e8b8ac5afaec2e3a2e02849454998e5ac
Carlos Damian <carlos.damian@choicehotels.com>
2023-12-26 23:11:36 -0600

    fix(deploy): fix next confifg

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.example.com/:path*',
+      },
+    ];
+  },
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': ['GET', 'POST', 'PUT', 'DELETE'],
