c982695601c8e395c8a1f9f04662862591da975c
Carlos Damian <carlos.damian@choicehotels.com>
2023-12-27 13:47:35 -0600

    fix(cors): trying to fix cors 2

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
     return [
       {
+        // matching all API routes
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://crud-next-eight.vercel.app/api/topics',
-          },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
           {
             key: 'Access-Control-Allow-Methods',
-            value: 'GET, DELETE, PATCH, POST, PUT, OPTIONS',
+            value: 'GET,DELETE,PATCH,POST,PUT',
           },
           {
             key: 'Access-Control-Allow-Headers',
